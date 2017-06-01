9e509f2918a4bea26a81a848b37752549c5e238f
rcortezk9 <rcortezk9@gmail.com>
2017-06-01 16:50:49 -0500

    added order by

--- sql/order_by_exercises.sql
+++ sql/order_by_exercises.sql
@@ -2,15 +2,23 @@ USE employees;
 
 SELECT *
 FROM employees
-WHERE gender = 'M'
-      AND (first_name ='Irena'
+WHERE first_name ='Irena'
+           OR first_name = 'Maya'
            OR first_name = 'Vidya'
-           OR first_name = 'Maya');
+ORDER BY first_name;
+
+SELECT *
+FROM employees
+WHERE first_name ='Irena'
+      OR first_name = 'Maya'
+      OR first_name = 'Vidya'
+ORDER BY first_name, last_name;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
-      OR last_name LIKE '%E';
+      OR last_name LIKE '%E'
+ORDER BY emp_no;
 
 SELECT *
 FROM employees
@@ -21,13 +29,6 @@ WHERE last_name LIKE 'E%'
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-      AND birth_date LIKE '%12-25';
+      AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC;
 
-SELECT *
-FROM employees
-WHERE birth_date LIKE '%12-25';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%'
-      AND last_name NOT LIKE '%qu%';
