b16d718f40b7b098a8ea09a0629f1ad7324f6de7
rcortezk9 <rcortezk9@gmail.com>
2017-06-01 15:48:27 -0500

    Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows. Now add a condition to find everybody with those names who is also male — 441 rows. Find all employees whose last name starts or ends with 'E' — 30,723 rows. Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows. Find all employees hired in the 90s and born on Christmas — 362 rows. Find all employees with a 'q' in their last name but not 'qu' — 547 rows.

--- sql/where_exercises.sql
+++ sql/where_exercises.sql
@@ -1,31 +1,41 @@
-# Be sure to select the employees database from the dropdown before you begin.
-# Write individual queries to find the following things:
 
-# Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
-# Employees whose last name starts with 'E' — 7,330 rows.
-# Employees hired in the 90s — 135,214 rows.
-# Employees born on Christmas — 842 rows.
-# Employees with a 'q' in their last name — 1,873 rows.
+# Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+# Now add a condition to find everybody with those names who is also male — 441 rows.
+# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+# Find all employees hired in the 90s and born on Christmas — 362 rows.
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
 
 USE employees;
 
-SELECT first_name, last_name
+SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE gender = 'M'
+AND (first_name ='Irena'
+      OR first_name = 'Vidya'
+      OR first_name = 'Maya');
 
-SELECT first_name, last_name
+SELECT *
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+      OR last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E';
 
 
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND birth_date LIKE '%12-25';
 
 SELECT *
 FROM employees
 WHERE birth_date LIKE '%12-25';
 
-SELECT first_name, last_name
+SELECT *
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+     AND NOT '%qu%';
