a6cfa687f7267581ec831073d7f397fd9c518b2c
rcortezk9 <rcortezk9@gmail.com>
2017-06-02 13:55:34 -0500

    added number of days worked from hire date to now

--- sql/functions_exercises.sql
+++ sql/functions_exercises.sql
@@ -2,29 +2,19 @@ USE employees;
 
 SELECT count(*), gender
 FROM employees
-WHERE (
-  first_name = 'Irena'
-  OR first_name = 'Maya'
-  OR first_name = 'Vidya'
-)
+WHERE first_name in ('Irena', 'Maya', 'Vidya')
 GROUP BY gender;
 
 
-SELECT concat(first_name, ' ', last_name)
+SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
-WHERE last_name LIKE 'E%'
-      OR last_name LIKE '%E'
-ORDER BY emp_no;
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-      AND last_name LIKE '%E';
+WHERE last_name LIKE 'E%E'; -- starts and ends
 
 
-SELECT first_name, last_name, hire_date
+SELECT
+  concat(first_name, ' ', last_name) AS 'full name',
+  datediff(curdate(), hire_date) AS 'number of days worked at Oracle'
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-      AND birth_date LIKE '%12-25'
-ORDER BY birth_date, hire_date DESC;
+      AND birth_date LIKE '%12-25';
 
