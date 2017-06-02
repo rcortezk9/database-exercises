a3b162cb2003161f327ee29c76b78360dfd2f9fc
rcortezk9 <rcortezk9@gmail.com>
2017-06-02 09:23:33 -0500

    selected by unique title and orded by title asc query 1st names that start and end with the letter e

---
+++ sql/group_by_exercise.sql
@@ -0,0 +1,16 @@
+USE employees;
+
+# In your script, use DISTINCT to find
+# the unique titles in the titles table.
+SELECT DISTINCT title
+FROM titles
+ORDER BY title ASC; # Update the previous query to sort the results alphabetically.
+
+# query for employees whose last names start and end with 'E'.
+# Update the query find just the unique last names that start and end with 'E' using GROUP BY.
+# The results should be:
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E';
