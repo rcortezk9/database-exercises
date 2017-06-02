61bc8e5e0438c73d512fd72c6e4331841c66cf0c
rcortezk9 <rcortezk9@gmail.com>
2017-06-02 10:43:41 -0500

    query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees

--- sql/limit_exercises.sql
+++ sql/limit_exercises.sql
@@ -5,3 +5,11 @@ FROM employees
 WHERE last_name LIKE 'Z%'
 ORDER BY last_name DESC
 LIMIT 10;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
