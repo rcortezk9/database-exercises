15fd505e813714c549effeaeeaa7e036f1c6c578
rcortezk9 <rcortezk9@gmail.com>
2017-06-02 14:20:38 -0500

    query concat name and got count of emp that hired in the 90's and birthdate of 12/25

--- sql/functions_exercises.sql
+++ sql/functions_exercises.sql
@@ -18,3 +18,11 @@ FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
       AND birth_date LIKE '%12-25';
 
+
+SELECT concat(first_name, ' ', last_name) AS 'full name', count(*)
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%'
+GROUP BY concat(first_name, ' ', last_name) DESC
+ORDER BY count(*) DESC;
+
