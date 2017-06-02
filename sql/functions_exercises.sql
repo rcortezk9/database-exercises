b6b6a782d233383a9577c0fbe8bcd49a25cf8443
rcortezk9 <rcortezk9@gmail.com>
2017-06-02 11:38:49 -0500

    saved order by ex to function ex

---
+++ sql/functions_exercises.sql
@@ -0,0 +1,34 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name ='Irena'
+      OR first_name = 'Maya'
+      OR first_name = 'Vidya'
+ORDER BY first_name;
+
+SELECT *
+FROM employees
+WHERE first_name ='Irena'
+      OR first_name = 'Maya'
+      OR first_name = 'Vidya'
+ORDER BY first_name, last_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+      OR last_name LIKE '%E'
+ORDER BY emp_no;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E';
+
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC;
+
